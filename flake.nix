aa982d77ea46488de3a3665e324dfb95434fc4e1
Takahito Nakano <kano@ponko2.jp>
2026-09-21 02:55:49 +0900

    chore(nix): update settings

--- flake.nix
+++ flake.nix
@@ -73,12 +73,6 @@
             ];
           };
           formatter = pkgs.nixfmt-tree;
-          packages = {
-            inherit (pkgs)
-              direnv
-              nix-direnv
-              ;
-          };
         };
     };
 }
